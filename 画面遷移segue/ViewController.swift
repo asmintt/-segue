c4ce62491f256a9f0437f69b85dd858aea9b9214
asamitakahashi <1105asamit@i.softbank.jp>
2020-07-04 15:20:04 +0900

    Initial Commit

---
+++ 画面遷移segue/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  画面遷移segue
+//
+//  Created by user on 2020/07/04.
+//  Copyright © 2020 asamin.t. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
