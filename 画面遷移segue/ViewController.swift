1696158a87492a6cf3521863342c5e53de49c654
asamitakahashi <1105asamit@i.softbank.jp>
2020-07-04 17:21:57 +0900

    レッスン4の8.2画面遷移　合計を表示　まで入力した

--- 画面遷移segue/ViewController.swift
+++ 画面遷移segue/ViewController.swift
@@ -14,7 +14,17 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?){
+        // segueから遷移先のResultViewControllerを取得する
+        let resultViewController:ResultViewController = segue.destination as!
+        ResultViewController
+        // 遷移先のResultViewControllerで宣言しているx，ｙに価を代入して渡す
+        resultViewController.x = 1
+        resultViewController.y = 1
+    }
 
-
+    @IBAction func unwind(_ segue: UIStoryboardSegue) {
+        // 他の画面からsegueを使って戻ってきた時に呼ばれる
+    }
 }
-
